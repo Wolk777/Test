c5bfbb250170ec5df0824176dbadeeceb28b306f
Wolk777 <Sokol.Vlad90@mail.ru>
2020-01-20 17:18:08 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector('h1');
+console.dir(h1);
+h1.style.color = 'purple';
+let h2 = document.querySelector('h2');
+h2.style.fontSize = '32px';
+h2.style.color = 'grey';
+h2.style.border = '2px solid orange';
