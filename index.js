9ae731866cf4b25426d9dfee4f6e00e5585e0a2a
Wolk777 <Sokol.Vlad90@mail.ru>
2020-01-20 11:27:51 +0300

    changed color

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let h1 = document.querySelector('h1');
 console.dir(h1);
 h1.style.color = 'purple';
+let h2 = document.querySelector('h2');
+h2.style.fontSize = '32px';
+h2.style.color = 'grey';
+h2.style.border = '1px solid yellow';
